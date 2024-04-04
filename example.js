d01a844e1c916da837e12938162a237df8d05894
lng2020 <nanguanlin6@gmail.com>
2024-04-05 01:44:45 +0800

    Fix concole naming

--- example.js
+++ example.js
@@ -1,17 +1,17 @@
-console.log("Hello", "runjs!");
-console.error("Error", "runjs!");
+my_console.log("Hello", "runjs!");
+my_console.error("Error", "runjs!");
 
 const path = "./log.txt";
 try {
     const contents = await extension.readFile(path);
-    console.log("Read from a file", contents);
+    my_console.log("Read from a file", contents);
 } catch (e) {
-    console.error("Error reading from a file", path, e);
+    my_console.error("Error reading from a file", path, e);
 }
 
 await extension.writeFile(path, "I can write to a file!");
 const contents = await extension.readFile(path);
-console.log("Read from a file", path, "contents:", contents);
-console.log("Removing file", path);
+my_console.log("Read from a file", path, "contents:", contents);
+my_console.log("Removing file", path);
 extension.removeFile(path);
-console.log("File removed", path);
+my_console.log("File removed", path);
--- runtime.js
+++ runtime.js
@@ -1,16 +1,14 @@
-((globalThis) => {
-    const core = Deno.core;
+const core = Deno.core;
 
-    function argsToMessage(...args) {
-        return args.map((arg) => JSON.stringify(arg)).join(" ");
-    }
+function argsToMessage(...args) {
+    return args.map((arg) => JSON.stringify(arg)).join(" ");
+}
 
-    globalThis.console = {
-        log: (...args) => {
-            core.print(`[OUT]: ${argsToMessage(...args)}\n`, false);
-        },
-        error: (...args) => {
-            core.print(`[ERR]: ${argsToMessage(...args)}\n`, true);
-        },
-    }
-})
+globalThis.my_console = {
+    log: (...args) => {
+        core.print(`[OUT]: ${argsToMessage(...args)}\n`, false);
+    },
+    error: (...args) => {
+        core.print(`[ERR]: ${argsToMessage(...args)}\n`, true);
+    },
+}
